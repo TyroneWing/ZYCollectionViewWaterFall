e72a48f731c1791bf32d0d1420d4246340faa13b
yi <zhengyi@tinckay.com>
2016-03-09 09:29:15 +0800

    Initial Commit

---
+++ ZYCollectionViewWaterFall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYCollectionViewWaterFall
+//
+//  Created by yi on 16/3/9.
+//  Copyright © 2016年 yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYCollectionViewWaterFall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYCollectionViewWaterFall
+//
+//  Created by yi on 16/3/9.
+//  Copyright © 2016年 yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
